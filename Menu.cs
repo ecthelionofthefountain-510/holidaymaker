377e7fedb2f34c53af2c8b0b45c67a301c97d394
Hultberg80 <hultberg80@gmail.com>
2024-11-27 11:56:35 +0100

    features?

--- Menu.cs
+++ Menu.cs
@@ -10,16 +10,51 @@ public class Menu
         _actions = actions;
         PrintMenu();
     }
-
+    
+    /* Skapa bokning
+     case 1. 
+     registrera kunden
+     
+     Vart vill du åka?
+     svar: land
+     
+     Vilken stad?
+     svar: stad
+     
+     När vill du åka?
+     svar: 
+     
+     Välj hotell:
+     svar: hotell
+     
+     Visa lediga rum
+     Filtrera / avstånd till centrum / avstånd till strand
+     / pris lågt-högt / about (pool, restaurant osv)
+     
+     Välj tillägg
+     
+     case 2. visa bokning
+     case 3. ändra bokning
+     case 4. boka!
+     case 5. ta bort bokning
+*/
     private void PrintMenu()
     {
-        Console.WriteLine("Choose option");
-        Console.WriteLine("1. List all");
-        Console.WriteLine("2. Show one");
-        Console.WriteLine("3. Add one");
-        Console.WriteLine("4. Update one");
-        Console.WriteLine("5. Delete one");
-        Console.WriteLine("9. Quit");
+        // Features
+        Console.WriteLine("Boka resa");
+        Console.WriteLine("1. Registrera kund/kunder");
+        Console.WriteLine("2. Välj land?");
+        Console.WriteLine("3. Välj hotell");
+        Console.WriteLine("4. Filtrera på ratings");
+        Console.WriteLine("5. Sök på boenden baserat på avstånd till centrum");
+        Console.WriteLine("6. Sök på boenden baserat på avstånd till havet");
+        Console.WriteLine("7. Tilläggsval (additional_choices)");
+        Console.WriteLine("8. List customers (antal personer, för- och efternamn, email, telefon, födelsedata)");
+        Console.WriteLine("9. Söka boenden baserat på pris (lågt till högt)");
+        Console.WriteLine("10. Sök lediga rum med specificerade sökkriterier");
+        Console.WriteLine("11. Ändra bokning");
+        Console.WriteLine("12. Boka!");
+        Console.WriteLine("0. Quit");
         AskUser();
     }
 
