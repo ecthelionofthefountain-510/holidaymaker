bcdb938bd8a4aead52f476914381e3dae177cec2
Hultberg80 <hultberg80@gmail.com>
2024-11-30 11:33:34 +0100

    Menu commit

--- Menu.cs
+++ Menu.cs
@@ -10,16 +10,16 @@ public class Menu
         _actions = actions;
         PrintMenu();
     }
-
+    
     private void PrintMenu()
     {
-        Console.WriteLine("Choose option");
-        Console.WriteLine("1. Register Customer");
-        Console.WriteLine("2. Show one");
-        Console.WriteLine("6. List contries");
-        Console.WriteLine("4. Update one");
-        Console.WriteLine("5. Delete one");
-        Console.WriteLine("9. Quit");
+        // Features
+        Console.WriteLine("Boka resa"); 
+        Console.WriteLine("1. Registrera kund/kunder");
+        Console.WriteLine("2. Search available rooms");
+        Console.WriteLine("3. Choose room and extra options");
+        Console.WriteLine("4. Show and save booking");
+        Console.WriteLine("5. Avsluta");
         AskUser();
     }
 
@@ -36,43 +36,15 @@ public class Menu
                     _actions.RegCustomer();
                     break;
                 case("2"):
-                    Console.WriteLine("Enter id to show details about one");
-                    id = Console.ReadLine();
-                    if (id is not null)
-                    { 
-                        _actions.ShowOne(id);
-                    }
+                    _actions.SearchRooms();
                     break;
                 case("3"):
-                    Console.WriteLine("Enter name (required)");
-                    var name = Console.ReadLine(); // required
-                    Console.WriteLine("Enter slogan");
-                    var slogan = Console.ReadLine(); // not required
-                    if (name is not null)
-                    {
-                        _actions.AddOne(name, slogan);
-                    }
+                    _actions.AddRoomAndOptions();
                     break;
                 case("4"):
-                    Console.WriteLine("Enter id to update one");
-                    id = Console.ReadLine();
-                    if (id is not null)
-                    { 
-                        _actions.UpdateOne(id);
-                    }
+                    _actions.SaveBooking();
                     break;
                 case("5"):
-                    Console.WriteLine("Enter id to delete one");
-                    id = Console.ReadLine();
-                    if (id is not null)
-                    { 
-                        _actions.DeleteOne(id);
-                    }
-                    break;
-                case"6":
-                    _actions.ListCountries();
-                    break;
-                case("9"):
                     Console.WriteLine("Quitting");
                     Environment.Exit(0);
                     break;
