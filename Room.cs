6a3995e9ce00cee3fc5334f08f3790aef1f5b83e
Hultberg80 <hultberg80@gmail.com>
2024-12-03 11:11:03 +0100

    changes to addroom

---
+++ Room.cs
@@ -0,0 +1,8 @@
+﻿namespace holidaymaker;
+
+public class Room
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long AccommodationId { get; set; }
+}
