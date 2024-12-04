29bbde6026a36c1cc514c4c0e3f7931f52865981
Hultberg80 <hultberg80@gmail.com>
2024-12-04 14:52:18 +0100

    ...

--- Room.cs
+++ Room.cs
@@ -2,7 +2,9 @@
 
 public class Room
 {
+    public int CustomerId { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public long AccommodationId { get; set; }
+    public int BookingId { get; set; }
 }
